2e36a8b3071cfc32e3fb8fedf39142b7c34223b7
fr-daria <123357433+fr-daria@users.noreply.github.com>
2023-06-18 19:03:20 +0300

    Add files via upload

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,18 +1,49 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
+import static java.util.Objects.requireNonNull;
+
 public class BooleanSearchEngine implements SearchEngine {
-    //???
+    private final Map<String, List<PageEntry>> database = new HashMap<>();
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
-        // прочтите тут все pdf и сохраните нужные данные,
-        // тк во время поиска сервер не должен уже читать файлы
+        for (File pdf : requireNonNull(pdfsDir.listFiles())) {
+            var doc = new PdfDocument(new PdfReader(pdf));
+            int pageCount = doc.getNumberOfPages();
+            for (int i = 1; i <= pageCount; i++) {
+                var page = doc.getPage(i);
+                var text = PdfTextExtractor.getTextFromPage(page);
+                var words = text.split("\\P{IsAlphabetic}+");
+                Map<String, Integer> freqs = new HashMap<>();
+                for (var word : words) { 
+                    if (word.isEmpty()) {
+                        continue;
+                    }
+                    word = word.toLowerCase();
+                    freqs.put(word, freqs.getOrDefault(word, 0) + 1);
+                }
+                for (String word : freqs.keySet()) {
+                    PageEntry pageEntry = new PageEntry(pdf.getName(), i, freqs.get(word));
+                    if (database.containsKey(word)) {
+                        database.get(word).add(pageEntry);
+                    } else {
+                        database.put(word, new ArrayList<>());
+                        database.get(word).add(pageEntry);
+                    }
+                }
+            }
+        }
     }
 
     @Override
     public List<PageEntry> search(String word) {
-        // тут реализуйте поиск по слову
-        return Collections.emptyList();
+        List<PageEntry> result = database.get(word);
+        Collections.sort(result);
+        return result;
     }
 }
--- src/main/java/PageEntry.java
+++ src/main/java/PageEntry.java
@@ -1,7 +1,70 @@
-public class PageEntry implements Comparable<PageEntry> {
-    private final String pdfName;
-    private final int page;
-    private final int count;
+import java.util.Objects;
 
-    // ???
+public class PageEntry implements Comparable<PageEntry> {
+    private String pdfName;
+    private int page;
+    private int count;
+
+    public PageEntry() {
+    }
+
+    public PageEntry(String pdfName, int page, int count) {
+        this.pdfName = pdfName;
+        this.page = page;
+        this.count = count;
+    }
+
+    public String getPdfName() {
+        return pdfName;
+    }
+
+    public int getPage() {
+        return page;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+
+    @Override
+    public String toString() {
+        return "\nPageEntry {\n " +
+                "pdfName = '" + pdfName + '\'' +
+                ",\n page = " + page +
+                ",\n count = " + count +
+                "\n}";
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PageEntry)) return false;
+        PageEntry pageEntry = (PageEntry) o;
+        return getPage() == pageEntry.getPage() && getCount() == pageEntry.getCount() && Objects.equals(getPdfName(), pageEntry.getPdfName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getPdfName(), getPage(), getCount());
+    }
+
+
+    @Override
+    public int compareTo(PageEntry o) {
+        if (count < o.count) {
+            return 1;
+        } else if (count > o.count) {
+            return -1;
+        } else {
+            if (page < o.page) {
+                return 1;
+            } else if (page > o.page) {
+                return -1;
+            } else {
+                return getPdfName().compareTo(o.getPdfName());
+            }
+        }
+    }
 }
